bc514467c3f2b5c60eb13575f05ad339e46f177c
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-12-12 15:06:48 +0100

    Configure Makefile & remove redundant files

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 #include <string>
+#include "BitonicSort.hpp"
 
-void print(int *list, unsigned size) {
-    std::string output = "[";
-    for (size_t i = 0; i < size; i++) {
-        output += std::to_string(list[i]);
-        if(i < size-1) { // size-1 is the last elem of the array
-            output += ", ";
-        }
-    }
-    output += "]";
-    std::cout << output << std::endl;
-}
-
-void swap(int *a, int *b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void compare(bool up, int *list, unsigned size) {
-    unsigned dist = size/2;
-    for (size_t i = 0; i < dist; i++) {
-        if((list[i] > list[i+dist]) == up) {
-            swap(&list[i], &list[i+dist]);
-        }
-    }
-}
-
-void sort(bool up, int *list, unsigned size) {
-    if(size > 1) {
-        sort(true, list, size/2);
-        sort(false, list+size/2, size/2);
-        compare(up, list, size);
-    }
-}
 
 int main() {
 
